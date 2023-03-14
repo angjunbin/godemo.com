77a2ec3222e269ac03e89facc7f9a1cdd1910f3b
zhangjunbin <junbin@junbin-2.local>
2023-03-14 17:10:22 +0800

    main test update

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ func init() {
 }
 
 func main() {
+<<<<<<< Updated upstream
 	fmt.Println("hello java")
+=======
+	fmt.Println("hello go")
+>>>>>>> Stashed changes
 
 	defer func() {
 		fmt.Println("recover: ", recover())
